942bce0824642b7d2047608496009565d5ce39d4
AnthonyJustice819 <tonyjustice819@gmail.com>
2025-07-16 08:03:17 +0100

    added the home section

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ const navlist = document.querySelector('nav .navList');
 console.log(closebtn);
 
 menubtn.addEventListener('click', (e)=> {
-if (e.target.id === 'menuIcon' && navlist.style.right === '-100%') {
+if (e.target.id === 'menuIcon' && navlist.style.right === '-105%') {
     navlist.style.right ='-50%';
     menubtn.style.display = 'none';
     // menubtn.style.transition = '0.3s ease';
     closebtn.style.display = 'block';
 } else {
-    navlist.style.right = '-100%';
+    navlist.style.right = '-105%';
 }
 })
 
 closebtn.addEventListener('click', (e)=> {
     if (e.target.id === 'closeIcon' && navlist.style.right === '-50%') {
-        navlist.style.right = '-100%';
+        navlist.style.right = '-105%';
         closebtn.style.display = 'none';
         menubtn.style.display = 'block';
     } else {
