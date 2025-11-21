0f40b7598adac49f985fc24e30a77cc232acfda8
AnthonyJustice819 <tonyjustice819@gmail.com>
2025-11-22 00:20:15 +0100

    edited the about section

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const menubtn = document.querySelector('#menuIcon');
 const closebtn = document.querySelector('#closeIcon');
 const navlist = document.querySelector('nav .navList');
-console.log(closebtn);
+
 
 menubtn.addEventListener('click', (e)=> {
 if (e.target.id === 'menuIcon' && navlist.style.right === '-105%') {
     navlist.style.right ='-50%';
     menubtn.style.display = 'none';
-    // menubtn.style.transition = '0.3s ease';
     closebtn.style.display = 'block';
 } else {
     navlist.style.right = '-105%';
